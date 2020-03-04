561565f7222faf355548ff1dcb499221b4ea9050
Luo.Xin <Xin.Luo@stratus.com>
2020-03-04 11:41:09 +0900

    springcloud netflix hystrix

--- springcloud-eureka-server/src/main/java/com/akakour/lab/eurekaserver/EurekaServer.java
+++ springcloud-eureka-server/src/main/java/com/akakour/lab/eurekaserver/EurekaServer.java
@@ -2,7 +2,9 @@ package com.akakour.lab.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,5 +13,4 @@ public class EurekaServer {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
     }
-
 }
