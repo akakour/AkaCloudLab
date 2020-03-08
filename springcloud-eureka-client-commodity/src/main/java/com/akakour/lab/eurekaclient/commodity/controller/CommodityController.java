9d3cddc25fb21b3e0411ef9fa14fd748efd89bce
Akakour <akakour@qq.com>
2020-03-08 15:22:28 +0900

    SpringCloud netflix config config-repo

--- springcloud-eureka-client-commodity/src/main/java/com/akakour/lab/eurekaclient/commodity/controller/CommodityController.java
+++ springcloud-eureka-client-commodity/src/main/java/com/akakour/lab/eurekaclient/commodity/controller/CommodityController.java
@@ -2,13 +2,26 @@ package com.akakour.lab.eurekaclient.commodity.controller;
 
 import com.akakour.lab.eurekaclient.commodity.dto.CommodityBean;
 import com.akakour.lab.eurekaclient.commodity.service.CommodityServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/commodity")
 @RestController
+//@Scope("refresh") 配合@value使用才能及时刷新配置
+@Scope("refresh")
 public class CommodityController {
+
+    @Value("${redis.password：1234567}")
+    private String redispw;
+
+    @Autowired
+    Environment env;
 
     @Autowired
     CommodityServiceImpl orderService;
@@ -25,5 +38,13 @@ public class CommodityController {
 
         CommodityBean commodityBean = new CommodityBean();
         return orderService.addToOrderBySemaphroe(commodityBean);
+    }
+
+    @RequestMapping("/config")
+    public String config() {
+        log.info("----springcloud-config： 【@Value redis.password】：" + redispw);
+        log.info("----springcloud-config： 【Environment redis.password】：" +
+                env.getProperty("redis.password"));
+        return "success";
     }
 }
