b3dbe3d5d9c1604c4caf73898b90739b3046b280
Akakour <akakour@qq.com>
2020-03-08 15:33:02 +0900

    clean up

--- springcloud-eureka-client-commodity/src/main/java/com/akakour/lab/eurekaclient/commodity/api/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.akakour.lab.eurekaclient.commodity.api;
-
-import com.akakour.lab.eurekaclient.commodity.dto.CommodityBean;
-
-public interface OrderService {
-    CommodityBean addToOrder(CommodityBean commodityBean);
-}
