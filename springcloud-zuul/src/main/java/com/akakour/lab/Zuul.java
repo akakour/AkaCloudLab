30c6d7096edfcd4cc70897a7e891b55f6112cf0e
Akakour <akakour@qq.com>
2020-03-11 00:51:23 +0900

    springcloud zuul 网关

---
+++ springcloud-zuul/src/main/java/com/akakour/lab/Zuul.java
@@ -0,0 +1,28 @@
+package com.akakour.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class Zuul {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul.class, args);
+    }
+
+    @Bean
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    @Primary
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+
+}
